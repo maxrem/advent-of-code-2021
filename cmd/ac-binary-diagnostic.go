77401d67381041ca86302ac1ad0f7a99416c92d4
maxrem <max.remkes@gmail.com>
2022-04-10 10:46:39 +0200

    ac binary diagnostic 1

---
+++ cmd/ac-binary-diagnostic.go
@@ -0,0 +1,97 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/maxrem/advent-of-code-2021/lib/io"
+	"github.com/spf13/cobra"
+)
+
+var binaryDiagnosticCmd = &cobra.Command{
+	Use:   "binary-diagnostic",
+	Short: "Binary diagnostic",
+	Run: func(cmd *cobra.Command, args []string) {
+		ch := make(chan string)
+		fileReader := io.NewFileReader("ac-binary-diagnostic", false)
+
+		diagnosticReader := DiagnosticReader{
+			ch:       ch,
+			counters: make([]Iets, 12, 12),
+		}
+
+		go fileReader.Read(ch)
+
+		diagnosticReader.HandleFirst()
+		diagnosticReader.Print()
+		gamma := diagnosticReader.GetBinary(true)
+		epsilon := diagnosticReader.GetBinary(false)
+		answer := diagnosticReader.GetUint(gamma) * diagnosticReader.GetUint(epsilon)
+		fmt.Printf("Answer is %d\n", answer)
+
+		// https://go.dev/play/p/lTKJdLxCIUT
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(binaryDiagnosticCmd)
+}
+
+type Iets struct {
+	ZeroCount int
+	OneCount int
+}
+
+type DiagnosticReader struct {
+	ch chan string
+	counters []Iets
+}
+
+func (r *DiagnosticReader) Print() {
+	for index, counter := range r.counters {
+		fmt.Println(fmt.Sprintf("Counter: %d, zero: %d, one: %d", index, counter.ZeroCount, counter.OneCount))
+	}
+}
+
+func (r *DiagnosticReader) GetBinary(isGamma bool) string {
+	zero := "0"
+	one := "1"
+	if !isGamma {
+		zero = "1"
+		one = "0"
+	}
+
+	var result string
+	for _, counter := range r.counters {
+		if counter.ZeroCount > counter.OneCount {
+			result += zero
+			continue
+		}
+
+		result += one
+	}
+
+	return result
+}
+
+func (r *DiagnosticReader) GetUint(binary string) uint64 {
+	i, err := strconv.ParseUint(binary, 2, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return i
+}
+
+func (r *DiagnosticReader) HandleFirst() {
+	for row := range r.ch {
+		for pos, char := range row {
+			if char == '0' {
+				r.counters[pos].ZeroCount++
+				continue
+			}
+			r.counters[pos].OneCount++
+		}
+	}
+}
